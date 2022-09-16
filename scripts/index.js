0195e0beea0f8d0cdd2a7e5e4b38e59d2cedc496
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2022-09-16 18:27:47 +0300

    feature: added js-functions for edit-profile form's validity

--- scripts/index.js
+++ scripts/index.js
@@ -100,12 +100,78 @@ function deleteCard(event) {
 }
 
 ///////////////////////////////////////////////////
-//Обработка события error при загрузки изображения
+//Обработка события error при загрузке изображения
 function loadDefaultImage(imageItem) {
   imageItem.setAttribute('src', './images/image-placeholder.jpg');
 }
+/****************************************************************************/
+// Функция отображения сообщения об ошибках
+function showInputError(formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input-text_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+}
+// Функция скрытия сообщения об ошибках
+function hideInputError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input-text_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+}
+// Функция для проверки валидности коллекции validity input-ов
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+// Функция для переключения состояния кнопки типа "submit"
+function toggleButtonState(inputList, buttonElement) {
+  if(hasInvalidInput(inputList)) {
+    buttonElement.classList.add('button_inactive');
+    buttonElement.setAttribute('disabled','disabled');
+  } else {
+    buttonElement.classList.remove('button_inactive');
+    buttonElement.removeAttribute('disabled');
+  }
+}
+// Функция для скрытия и отображения сообщения об ошибках
+function checkInputValidity(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}
+// Функция для добавления слушателя 'input' в каждое поле ввода формы
+function setEventListeners(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll(`.popup__input-text`));
+  // Найдём в текущей форме кнопку отправки
+  const buttonElement = formElement.querySelector('.button_type_submit');
+  //toggleButtonState(inputList, buttonElement);
 
-
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+}
+// Функция для обработки валидации всем формам
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    const fieldsetList = Array.from(formElement.querySelectorAll('.popup__fieldset'));
+    fieldsetList.forEach((fieldsetElement) => {
+      setEventListeners(fieldsetElement);
+    });
+  });
+}
+enableValidation();
+/****************************************************************************/
 // Обработка нажатия на изображение
 function openImage(event) {
   const listItem = event.target.closest('.gallery__item');
@@ -160,7 +226,7 @@ function handleCardFormSubmit(evt) {
   evt.target.reset();
 }
 cardPopupForm.addEventListener('submit', handleCardFormSubmit);
-////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 // закрытие модального окна по нажатию на Escape
 document.addEventListener('keydown', function (evt) {
   const popupOpened = document.querySelector('.popup_opened');
